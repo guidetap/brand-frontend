d25f135a6531001c2e09876cb18be77fcacb78ca
Tommy Gammon <tommygammon07@gmail.com>
2020-09-28 09:51:35 -0700

    fix section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import Header from './components/Header';
-import Home from './components/Home';
-import LogoClouds from './components/LogoClouds';
-import FeatureSections from './components/FeatureSections';
-import PricePlan from './components/PricePlan';
-import CTASection from './components/CTASection';
-import Footer from './components/Footer';
+import Header from 'components/Header';
+import Home from 'components/Home';
+import LogoClouds from 'components/LogoClouds';
+import FeatureSections from 'components/FeatureSections';
+import CTASection from 'components/CTASection';
+import Footer from 'components/Footer';
 
-import logo from './logo.png';
+import logo from 'logo.png';
 
 function App() {
   return (
