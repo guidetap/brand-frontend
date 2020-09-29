ca6b66631ba61184592b356829b1eca478707cfe
Tommy Gammon <tommygammon07@gmail.com>
2020-09-29 08:21:45 -0700

    Login & Register page

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,20 @@ import Footer from 'components/Footer';
 import Home from 'pages/Home';
 import PricePlanPage from 'components/PricePlan';
 import StripeCheckout from 'pages/StripeCheckout';
+import ConnectShopify from 'pages/ConnectShopify';
+import Login from 'pages/Login';
+import Register from 'pages/Register';
 
 function App() {
   return (
     <>
       <Header />
       <Switch>
-        <Route exact path="/" component={StripeCheckout} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/connect-shopify" component={ConnectShopify} />
+        <Route exact path="/stripe-checkout" component={StripeCheckout} />
         <Route exact path="/pricing" component={PricePlanPage} />
       </Switch>
       <Footer />
