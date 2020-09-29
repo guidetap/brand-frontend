696e468a85ab1d80c9b858e65d58c8f1165cccf1
Tommy Gammon <tommygammon07@gmail.com>
2020-09-29 07:22:02 -0700

    set absolute route

--- src/components/HomeBanner/index.js
+++ src/components/HomeBanner/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => (
+const HomeBanner = () => (
   <div className="relative bg-white overflow-hidden">
     <div className="max-w-screen-xl mx-auto">
       <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
@@ -35,4 +35,4 @@ const Home = () => (
   </div>
 );
 
-export default Home;
+export default HomeBanner;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Switch, Route, } from 'react-router-dom';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
 
-import LandingPage from '../pages/Landing';
-import PricePlanPage from '../components/PricePlan';
+import Home from 'pages/Home';
+import PricePlanPage from 'components/PricePlan';
+import StripeCheckout from 'pages/StripeCheckout';
 
 function App() {
   return (
     <>
       <Header />
       <Switch>
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/" component={StripeCheckout} />
         <Route exact path="/pricing" component={PricePlanPage} />
       </Switch>
       <Footer />
